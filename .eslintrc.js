a9ee7be78e5317bf1d98397dd360955ff411373b
Adnan Ademovic <adnanademovic100@gmail.com>
2021-08-10 19:29:15 +0200

    Initialize repository boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'airbnb-base',
+		'airbnb-base/rules/strict',
+		'plugin:prettier/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+	],
+	plugins: ['import'],
+	rules: {
+		'import/extensions': 'off',
+		'no-restricted-syntax': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts'],
+		},
+		'import/resolver': {
+			node: {
+				extensions: ['.ts'],
+				moduleDirectory: ['node_modules', 'src/'],
+			},
+			typescript: {},
+		},
+	},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	bracketSpacing: true,
+	semi: true,
+	jsxBracketSameLine: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	useTabs: true,
+};
