b858abefbef7265e34158605a9c62de98d81d769
Adnan Ademovic <adnanademovic100@gmail.com>
2021-08-11 12:59:14 +0200

    Test all functions

--- src/combiners.ts
+++ src/combiners.ts
@@ -38,15 +38,3 @@ export function objectOf<T>(
 	return (value): value is { [key: string]: T } =>
 		isObject(value) && Object.values(value).every(isChildType);
 }
-
-export function structure<T extends { [key: string]: unknown }>(
-	isChildTypes: {
-		[K in keyof Required<T>]: (value: unknown) => value is T[K];
-	},
-): (value: unknown) => value is T {
-	return (value): value is T =>
-		isObject(value) &&
-		Object.keys(isChildTypes).every((key) =>
-			isChildTypes[key as keyof T]?.(value[key]),
-		);
-}
