483308e39b3d46b5651988608b791f3474389bd6
Adnan Ademovic <adnanademovic100@gmail.com>
2021-08-12 13:51:52 +0200

    Split code into modules again

--- index.ts
+++ index.ts
@@ -1,87 +1,4 @@
-export const isUnknown = (value: unknown): value is unknown => true;
-
-export const isNull = (value: unknown): value is null => value === null;
-
-export const isUndefined = (value: unknown): value is undefined =>
-	value === undefined;
-
-export const isString = (value: unknown): value is string =>
-	typeof value === 'string';
-
-export const isNumber = (value: unknown): value is number =>
-	typeof value === 'number';
-
-export const isBoolean = (value: unknown): value is boolean =>
-	typeof value === 'boolean';
-
-export const isObject = (value: unknown): value is { [key: string]: unknown } =>
-	typeof value === 'object' && value !== null && !Array.isArray(value);
-
-export const isArray = (value: unknown): value is unknown[] =>
-	Array.isArray(value);
-
-export function oneOf<T extends unknown[]>(
-	...isChildTypes: { [P in keyof T]: (value: unknown) => value is T[P] }
-): (value: unknown) => value is T[number] {
-	return (value): value is T[number] =>
-		isChildTypes.some((check) => check(value));
-}
-
-export function literal<T extends string | number | boolean>(
-	...literals: readonly T[]
-): (value: unknown) => value is T {
-	return (value): value is T => literals.some((v) => v === value);
-}
-
-export function optional<T>(
-	isChildType: (value: unknown) => value is T,
-): (value: unknown) => value is T | undefined {
-	return oneOf(isUndefined, isChildType);
-}
-
-export function nullable<T>(
-	isChildType: (value: unknown) => value is T,
-): (value: unknown) => value is T | null {
-	return oneOf(isNull, isChildType);
-}
-
-export function arrayOf<T>(
-	isChildType: (value: unknown) => value is T,
-): (value: unknown) => value is T[] {
-	return (value): value is T[] => isArray(value) && value.every(isChildType);
-}
-
-export function objectOf<T>(
-	isChildType: (value: unknown) => value is T,
-): (value: unknown) => value is { [key: string]: T } {
-	return (value): value is { [key: string]: T } =>
-		isObject(value) && Object.values(value).every(isChildType);
-}
-
-export function structure<T extends { [key: string]: unknown }>(
-	isChildTypes: {
-		[K in keyof Required<T>]: (value: unknown) => value is T[K];
-	},
-): (value: unknown) => value is T {
-	return (value): value is T =>
-		isObject(value) &&
-		Object.keys(isChildTypes).every((key) =>
-			isChildTypes[key as keyof T]?.(value[key]),
-		);
-}
-
-export function tuple<T extends unknown[]>(
-	...isChildTypes: {
-		[K in keyof Required<T>]: (value: unknown) => value is T[K];
-	}
-): (value: unknown) => value is T {
-	return (value): value is T =>
-		isArray(value) &&
-		value.length === isChildTypes.length &&
-		isChildTypes.every((checker, idx) => checker(value[idx]));
-}
-
-export default {
+export {
 	isUnknown,
 	isNull,
 	isUndefined,
@@ -90,12 +7,15 @@ export default {
 	isBoolean,
 	isObject,
 	isArray,
+} from './src/basic';
+
+export {
 	oneOf,
 	literal,
 	optional,
 	nullable,
 	arrayOf,
 	objectOf,
-	structure,
-	tuple,
-};
+} from './src/combiners';
+
+export { structure, tuple } from './src/structure';
